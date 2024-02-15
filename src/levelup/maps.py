9ce83771f8f4923c57cbc983a8ac8d904e778f79
Nathan Center <nathanjcenter@gmail.com>
2024-02-15 17:35:21 -0500

    calculate postition

--- src/levelup/maps.py
+++ src/levelup/maps.py
@@ -4,3 +4,13 @@ class GameMap:
         self.length = length
         self.starting_position = starting_position
     
+    def calculate_position(currentPosition, direction):
+        if direction == "NORTH":
+            position = (currentPosition[0], currentPosition[1] + 1)
+        if direction == "SOUTH":
+            position = (currentPosition[0], currentPosition[1] - 1)
+        if direction == "EAST":
+            position = (currentPosition[0] + 1, currentPosition[1])
+        if direction == "WEST":
+            position = (currentPosition[0] - 1, currentPosition[1])
+        return position
--- tests/test_maps.py
+++ tests/test_maps.py
@@ -1,9 +1,27 @@
 from unittest import TestCase
 from levelup.maps import GameMap
 
+
 class TestMap(TestCase):
     def test_init(self):
         testmap = GameMap(10,10,(5,5))
         assert testmap.length != None
         assert testmap.width != None
         assert testmap.starting_position != None
+    
+    def test_calculate_position_1(self):
+        position = GameMap.calculate_position((5,5), "NORTH")
+        assert position == (5,6)
+
+    def test_calculate_position_2(self):
+        position = GameMap.calculate_position((5,5), "SOUTH")
+        assert position == (5,4)
+
+    def test_calculate_position_3(self):
+        position = GameMap.calculate_position((5,5), "EAST")
+        assert position == (6,5)
+        
+    def test_calculate_position_4(self):
+        position = GameMap.calculate_position((5,5), "WEST")
+        assert position == (4,5)
+        
