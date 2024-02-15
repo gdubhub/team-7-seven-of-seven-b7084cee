58f3697c30d2d3f23fc6d846e7e912a3fe4efb69
Jylian Ramsey <jylianramsey@gmail.com>
2024-02-15 18:26:35 -0500

    removed comments

--- tests/test_position.py
+++ tests/test_position.py
@@ -2,9 +2,6 @@ from unittest import TestCase
 from unittest.mock import MagicMock, create_autospec
 from levelup.position import position
 
-# THIS IS AN EXAMPLE UNIT TEST. 
-# All the unit tests for the Game Controller should go here
-# Unit tests for other classes should be in their own .py files (like test_character.py)
 class TestPosition(TestCase):
     def test_init(self):
         testObj = position()
